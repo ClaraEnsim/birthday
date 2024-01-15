0883ee63d7540cbb64426eda2e0ee12aacb5dbc7
clara <clara.tournay.etu@univ-lemans.fr>
2024-01-15 10:31:16 +0100

    1er commit

---
+++ birthday.js
@@ -0,0 +1,13 @@
+function adressToParty() {
+    window.location.href = "https://www.lemans.fr/dynamique/la-proximite/les-salles-municipales/le-descriptif-des-salles-municipales/la-salle-du-jardin-des-plantes/";
+}
+
+function displayExplanations(){
+    var paragraph = document.getElementById("ExplainParty");
+    if (paragraph) {
+        paragraph.style.display = "block";
+    }
+}
+
+
+
