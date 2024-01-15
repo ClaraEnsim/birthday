acee4f354e31d140cde1c98d8d5916bc3c2cef1d
clara <clara.tournay.etu@univ-lemans.fr>
2024-01-15 12:32:36 +0100

    3e commit

--- birthday.php
+++ birthday.php
@@ -10,7 +10,7 @@
         <header>Anniversaire de Clara</header></br>
         <h1>sur le thème</h1>
         <h2>!! STAR 80 !!</h2>
-        <button onclick="displayExplanations()">en savoir plus</button>
+        <button id="party"onclick="displayExplanations()">en savoir plus</button>
         <p id="ExplainParty">Le thème de la fête : une excuse pour se déguiser!</br>
             Les comédies musicales ! </br>
             Saturday Night fever ! </br>
@@ -23,18 +23,20 @@
 
     <div id="adress">
         <h3>RDV le samedi 12 mai 2024 à 20h</h3>
-        <button onclick="adressToParty()">clique ici pour connaître l'adresse</button>
+        <button id="lieu"onclick="adressToParty()">clique ici pour connaître l'adresse</button>
     </div>
 
     <form method="post" action="register.php">
-        <h4>Formulaire</h12></br>
+        <h4>Formulaire : </h4></br>
         <input type="text" id="name" name="nom" placeholder="Nom"/></br>
         <input type="number" id="number" name="nombre" min="1" max="10"/></br>
-        <input type="submit" name="submit" value="Envoyer"></br>
+        <input id="envoyer"type="submit" name="submit" value="Envoyer"></br>
+    
+    </br>
+        <?php include "count_guests.php" ?>
+    </br>
     </form>
-</br>
-    <?php include "count_guests.php" ?>
-</br>
+
     <script src="birthday.js"></script>
 </body>
 </html>
